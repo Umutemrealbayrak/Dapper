cd8a85608d97cf79a5077b9376bed8860263f66f
Umutemrealbayrak <96690901+Umutemrealbayrak@users.noreply.github.com>
2024-11-21 15:46:06 +0300

    Add files via upload

---
+++ Dapper/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using RealEstateDapper.Dtos.CategoryDtos;
+using RealEstateDapper.Models.DapperContext;
+using Dapper;
+
+namespace RealEstateDapper.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "select * from Category";
+            using (var connection = _context.CreateConnection()) { 
+                var values=await connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList();
+            }
+        }   
+    }
+}
---
+++ Dapper/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstateDapper.Dtos.CategoryDtos;
+
+namespace RealEstateDapper.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();        
+    }
+}
